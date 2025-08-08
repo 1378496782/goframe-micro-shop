b34787154c3d6d8d044a77404812145286847da5
yemcry <yemcry@163.com>
2025-08-08 18:35:51 +0800

    双网关架构设计

---
+++ app/gateway-h5/api/user/user.go
@@ -0,0 +1,17 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package user
+
+import (
+	"context"
+
+	"shop-goframe-micro-service-refacotor/app/gateway-h5/api/user/v1"
+)
+
+type IUserV1 interface {
+	ConsigneeInfoGetList(ctx context.Context, req *v1.ConsigneeInfoGetListReq) (res *v1.ConsigneeInfoGetListRes, err error)
+	ConsigneeInfoUpdate(ctx context.Context, req *v1.ConsigneeInfoUpdateReq) (res *v1.ConsigneeInfoUpdateRes, err error)
+	ConsigneeInfoDelete(ctx context.Context, req *v1.ConsigneeInfoDeleteReq) (res *v1.ConsigneeInfoDeleteRes, err error)
+}
---
+++ app/gateway-h5/internal/cmd/cmd.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"context"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gcmd"
+	"shop-goframe-micro-service-refacotor/app/gateway-h5/internal/controller/user"
+)
+
+var (
+	Main = gcmd.Command{
+		Name:  "main",
+		Usage: "main",
+		Brief: "start http gateway-h5 server",
+		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+			s := g.Server()
+			s.Group("/", func(group *ghttp.RouterGroup) {
+				group.Middleware(ghttp.MiddlewareHandlerResponse)
+				group.Group("/frontend", func(group *ghttp.RouterGroup) {
+					group.Bind(user.NewV1())
+				})
+			})
+			s.Run()
+			return nil
+		},
+	}
+)
