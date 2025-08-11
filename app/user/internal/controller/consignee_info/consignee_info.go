d7702f04c9ae43035b68bc80b2023e54b5bbd16c
yemcry <yemcry@163.com>
2025-08-11 20:04:50 +0800

    GoFrame框架定制（统一错误处理+日志追踪）

--- app/user/internal/controller/consignee_info/consignee_info.go
+++ app/user/internal/controller/consignee_info/consignee_info.go
@@ -2,9 +2,13 @@ package consignee_info
 
 import (
 	"context"
+	"github.com/gogf/gf/v2/errors/gcode"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/util/gconv"
 	v1 "shop-goframe-micro-service-refacotor/app/user/api/consignee_info/v1"
 	"shop-goframe-micro-service-refacotor/app/user/api/pbentity"
+	"shop-goframe-micro-service-refacotor/app/user/internal/consts"
 	"shop-goframe-micro-service-refacotor/app/user/internal/dao"
 	"shop-goframe-micro-service-refacotor/app/user/internal/model/entity"
 	"shop-goframe-micro-service-refacotor/utility"
@@ -29,11 +33,14 @@ func (*Controller) GetList(ctx context.Context, req *v1.ConsigneeInfoGetListReq)
 		Size:  req.Size,
 		Total: 0,
 	}
-
+	// 错误类型
+	infoError := consts.InfoError(consts.ConsigneeInfo, consts.GetListFail)
 	// 查询总数
 	total, err := dao.ConsigneeInfo.Ctx(ctx).Count()
 	if err != nil {
-		return &v1.ConsigneeInfoGetListRes{Data: response}, nil
+		// 记录错误日志
+		g.Log().Errorf(ctx, "%v %v", infoError, err)
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
 	}
 	response.Total = uint32(total)
 
@@ -42,7 +49,8 @@ func (*Controller) GetList(ctx context.Context, req *v1.ConsigneeInfoGetListReq)
 		Page(int(req.Page), int(req.Size)).
 		All()
 	if err != nil {
-		return &v1.ConsigneeInfoGetListRes{Data: response}, nil
+		g.Log().Errorf(ctx, "%v %v", infoError, err)
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
 	}
 
 	// 数据转换
@@ -65,6 +73,7 @@ func (*Controller) GetList(ctx context.Context, req *v1.ConsigneeInfoGetListReq)
 
 		response.List = append(response.List, &pbConsignee)
 	}
+
 	return &v1.ConsigneeInfoGetListRes{Data: response}, nil
 }
 
@@ -76,11 +85,15 @@ func (*Controller) Create(ctx context.Context, req *v1.ConsigneeInfoCreateReq) (
 	if err := gconv.Struct(req, &consigneeInfo); err != nil {
 		return nil, err
 	}
+	// 错误类型
+	infoError := consts.InfoError(consts.ConsigneeInfo, consts.CreateFail)
 	// 向数据库中插入数据并获取自动生成的ID
 	result, err := dao.ConsigneeInfo.Ctx(ctx).InsertAndGetId(req)
 	if err != nil {
-		return nil, err
+		g.Log().Errorf(ctx, "%v %v", infoError, err)
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
 	}
+
 	// 返回创建成功响应，包含新创建的ID
 	return &v1.ConsigneeInfoCreateRes{Id: uint32(result)}, nil
 }
@@ -93,11 +106,14 @@ func (*Controller) Update(ctx context.Context, req *v1.ConsigneeInfoUpdateReq) (
 	if err := gconv.Struct(req, &consigneeInfo); err != nil {
 		return nil, err
 	}
+	infoError := consts.InfoError(consts.ConsigneeInfo, consts.UpdateFail)
 	// 根据ID更新数据库中的信息
 	_, err = dao.ConsigneeInfo.Ctx(ctx).Where("id", req.Id).Update(req)
 	if err != nil {
-		return nil, err
+		g.Log().Errorf(ctx, "%v %v", infoError, err)
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
 	}
+
 	// 返回更新成功响应，包含被更新ID
 	return &v1.ConsigneeInfoUpdateRes{Id: req.Id}, nil
 }
@@ -106,6 +122,12 @@ func (*Controller) Update(ctx context.Context, req *v1.ConsigneeInfoUpdateReq) (
 func (*Controller) Delete(ctx context.Context, req *v1.ConsigneeInfoDeleteReq) (res *v1.ConsigneeInfoDeleteRes, err error) {
 	// 根据ID从数据库中删除对应信息
 	_, err = dao.ConsigneeInfo.Ctx(ctx).Where("id", req.Id).Delete()
+	infoError := consts.InfoError(consts.ConsigneeInfo, consts.DeleteFail)
+	if err != nil {
+		g.Log().Errorf(ctx, "%v %v", infoError, err)
+		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+	}
+
 	// 返回删除成功的空响应
 	return &v1.ConsigneeInfoDeleteRes{}, nil // 返回空结构体
 }
