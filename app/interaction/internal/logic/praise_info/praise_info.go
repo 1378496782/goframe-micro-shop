b7a507b85fe096567a4950c974160d0ccadeb2ef
zhangfuwei.666 <zhangfuwei.666@bytedance.com>
2026-06-21 04:39:17 +0800

    docs(comment-praise): update doc and refactor praise logic to add transaction support
    refactor the praise info logic to wrap praise record and count update into same local transaction, update the related documentation to explain the changes and conclusions
    
    Change-Id: I9361ba9b2a2ea23dd399a8b429843b6e42be338e

--- app/interaction/internal/logic/praise_info/praise_info.go
+++ app/interaction/internal/logic/praise_info/praise_info.go
@@ -7,6 +7,7 @@ import (
 	"shop-goframe-micro-service-refacotor/utility/consts"
 	"strings"
 
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
@@ -23,31 +24,39 @@ func Create(ctx context.Context, req *v1.PraiseInfoCreateReq) (res *v1.PraiseInf
 	res = &v1.PraiseInfoCreateRes{}
 	// 错误类型
 	infoError := consts.InfoError(consts.PraiseInfo, consts.CreateFail)
-	if req.Type == PraiseTypeComment {
-		if err := ensureCommentLikeTarget(ctx, req.ObjectId); err != nil {
-			g.Log().Errorf(ctx, "%v %v", infoError, err)
-			return nil, err
-		}
-	}
-	// 向数据库中插入数据并获取自动生成的ID
-	id, err := dao.PraiseInfo.Ctx(ctx).InsertAndGetId(req)
-	if err != nil {
-		if isDuplicatePraiseError(err) {
-			res.Id = uint32(id)
-			return res, nil
-		}
-		g.Log().Errorf(ctx, "%v %v", infoError, err)
-		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
-	}
 
-	if req.Type == PraiseTypeArticle {
-		// TODO文章点赞
-	} else if req.Type == PraiseTypeComment {
-		// 评论/回复点赞
-		if err := increaseCommentLikeCount(ctx, req.ObjectId); err != nil {
-			g.Log().Errorf(ctx, "%v %v", infoError, err)
-			return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+	var id int64
+	err = dao.PraiseInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		if req.Type == PraiseTypeComment {
+			if err := ensureCommentLikeTargetTx(ctx, tx, req.ObjectId); err != nil {
+				g.Log().Errorf(ctx, "%v %v", infoError, err)
+				return err
+			}
 		}
+		// 向数据库中插入数据并获取自动生成的ID
+		id, err = tx.Model(dao.PraiseInfo.Table()).InsertAndGetId(req)
+		if err != nil {
+			if isDuplicatePraiseError(err) {
+				res.Id = uint32(id)
+				return nil
+			}
+			g.Log().Errorf(ctx, "%v %v", infoError, err)
+			return gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+		}
+
+		if req.Type == PraiseTypeArticle {
+			// TODO文章点赞
+		} else if req.Type == PraiseTypeComment {
+			// 评论/回复点赞
+			if err := increaseCommentLikeCountTx(ctx, tx, req.ObjectId); err != nil {
+				g.Log().Errorf(ctx, "%v %v", infoError, err)
+				return gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	res.Id = uint32(id)
@@ -64,51 +73,57 @@ func isDuplicatePraiseError(err error) bool {
 
 func Delete(ctx context.Context, req *v1.PraiseInfoDeleteReq) (res *v1.PraiseInfoDeleteRes, err error) {
 	res = &v1.PraiseInfoDeleteRes{}
-	// 根据ID从数据库中删除对应信息
-	result, err := dao.PraiseInfo.Ctx(ctx).Where(g.Map{
-		"id":        req.Id,
-		"user_id":   req.UserId,
-		"type":      req.Type,
-		"object_id": req.ObjectId,
-	}).Delete()
-	infoError := consts.InfoError(consts.PraiseInfo, consts.DeleteFail)
-	if err != nil {
-		g.Log().Errorf(ctx, "%v %v", infoError, err)
-		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
-	}
-	rows, err := result.RowsAffected()
-	if err != nil {
-		g.Log().Errorf(ctx, "%v %v", infoError, err)
-		return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
-	}
-	res.Id = req.Id
-	if rows == 0 {
-		return
-	}
-
-	if req.Type == PraiseTypeArticle {
-		// TODO文章点赞
-	} else if req.Type == PraiseTypeComment {
-		// 评论/回复点赞
-		if err := decreaseCommentLikeCount(ctx, req.ObjectId); err != nil {
+	txErr := dao.PraiseInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		result, err := tx.Model(dao.PraiseInfo.Table()).
+			Where(g.Map{
+				"id":        req.Id,
+				"user_id":   req.UserId,
+				"type":      req.Type,
+				"object_id": req.ObjectId,
+			}).Delete()
+		infoError := consts.InfoError(consts.PraiseInfo, consts.DeleteFail)
+		if err != nil {
 			g.Log().Errorf(ctx, "%v %v", infoError, err)
-			return nil, gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+			return gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
 		}
-	}
+		rows, err := result.RowsAffected()
+		if err != nil {
+			g.Log().Errorf(ctx, "%v %v", infoError, err)
+			return gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+		}
+		res.Id = req.Id
+		if rows == 0 {
+			return nil
+		}
 
+		if req.Type == PraiseTypeArticle {
+			// TODO文章点赞
+		} else if req.Type == PraiseTypeComment {
+			// 评论/回复点赞
+			if err := decreaseCommentLikeCountTx(ctx, tx, req.ObjectId); err != nil {
+				g.Log().Errorf(ctx, "%v %v", infoError, err)
+				return gerror.WrapCode(gcode.CodeDbOperationError, err, infoError)
+			}
+		}
+
+		return nil
+	})
+	if txErr != nil {
+		return nil, txErr
+	}
 	return
 }
 
-func increaseCommentLikeCount(ctx context.Context, commentId uint32) error {
-	_, err := dao.CommentInfo.Ctx(ctx).
+func increaseCommentLikeCountTx(ctx context.Context, tx gdb.TX, commentId uint32) error {
+	_, err := tx.Model(dao.CommentInfo.Table()).
 		Where(dao.CommentInfo.Columns().Id, commentId).
 		WhereNull(dao.CommentInfo.Columns().DeletedAt).
 		Increment(dao.CommentInfo.Columns().LikeCount, 1)
 	return err
 }
 
-func ensureCommentLikeTarget(ctx context.Context, commentId uint32) error {
-	count, err := dao.CommentInfo.Ctx(ctx).
+func ensureCommentLikeTargetTx(ctx context.Context, tx gdb.TX, commentId uint32) error {
+	count, err := tx.Model(dao.CommentInfo.Table()).
 		Where(dao.CommentInfo.Columns().Id, commentId).
 		WhereNull(dao.CommentInfo.Columns().DeletedAt).
 		Count()
@@ -121,8 +136,8 @@ func ensureCommentLikeTarget(ctx context.Context, commentId uint32) error {
 	return nil
 }
 
-func decreaseCommentLikeCount(ctx context.Context, commentId uint32) error {
-	_, err := dao.CommentInfo.Ctx(ctx).
+func decreaseCommentLikeCountTx(ctx context.Context, tx gdb.TX, commentId uint32) error {
+	_, err := tx.Model(dao.CommentInfo.Table()).
 		Where(dao.CommentInfo.Columns().Id, commentId).
 		WhereGT(dao.CommentInfo.Columns().LikeCount, 0).
 		WhereNull(dao.CommentInfo.Columns().DeletedAt).
